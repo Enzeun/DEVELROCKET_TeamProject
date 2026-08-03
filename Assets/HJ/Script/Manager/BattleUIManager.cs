1a5d0286960af2add8f48112cb962c3d61690176
Enzeun <khuynjun123@naver.com>
2026-08-03 16:46:22 +0900

    [feat] 피 닳때 이펙트 추가

--- Assets/HJ/Script/Manager/BattleUIManager.cs
+++ Assets/HJ/Script/Manager/BattleUIManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Unity.UI;
 using UnityEngine.UI;
 using DG.Tweening;
+using DamageNumbersPro;
 
 public class BattleUIManager : MonoBehaviour
 {
@@ -11,7 +12,10 @@ public class BattleUIManager : MonoBehaviour
     private TextMeshProUGUI playerHpBarText;
     [SerializeField, Required, BoxGroup("**참조필요!**")]
     private Slider playerHpSlider;
-
+    [SerializeField, Required, BoxGroup("**참조필요!**")]
+    private DamageNumber numberPrefab;
+    [SerializeField, Required, BoxGroup("**참조필요!**")]
+    private RectTransform rectParent;
 
     private PlayerBaseStat playerStat;
 
@@ -33,6 +37,8 @@ public class BattleUIManager : MonoBehaviour
     [BoxGroup("UI Debug"), Button]
     private void PlayDamagedUIAnimation(int damage, float endValue)
     {
+        DamageNumber damageNumber = numberPrefab.SpawnGUI(rectParent, Vector2.zero, damage);
+
         playerHpSlider.transform.DOShakePosition(0.2f, 10f, 90);
         playerHpSlider.DOValue(endValue, 0.1f);
     }
