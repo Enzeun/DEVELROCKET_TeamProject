eb6425e6fbf31d3effab6ff64909a171b8bccc14
RGovernment <117322410+RGovernment@users.noreply.github.com>
2026-08-03 10:44:39 +0900

    [feat] 스킬 기본 구조 추가
    1. 스킬 기본 구조 추가
    ㄴ 1-2. 스킬 기본 구조(아이디,이름, 비용,피해, 업그레이드 가능 여부, 현재 업그레이드, 대상타입(단일, 범위))로 지정
    ㄴ 1-3. 스킬 피해 계산, 스킬 사용시 필요한 데이터만 반환하는 함수(UseSkillData) 추가

--- Assets/JJH/Skill/SkillBaseStat.cs
+++ Assets/JJH/Skill/SkillBaseStat.cs
@@ -1,6 +1,49 @@
+using System.Collections.Generic;
 using UnityEngine;
+using static SkillEnums;
 
-public class SkillBaseStat : MonoBehaviour
+public class SkillBaseStat
 {
+    public int Id;
     public string Name;
+    public string Description;
+    private int Cost;
+    private int Damage;
+    
+    public List<SkillUpgradeType> UpgradeAbleList;
+    public List<SkillUpgradeType> NowUpgradeList;
+    public SkillTargetType TargetType;
+
+    public SkillBaseStat(int id, string name, string description, int cost, int damage, 
+        List<SkillUpgradeType> upgradeAbleList, SkillTargetType targetType)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Cost = cost;
+        Damage = damage;
+        UpgradeAbleList = upgradeAbleList;
+        NowUpgradeList = new();
+        TargetType = targetType;
+    }
+
+    public (int skillCost, int SkillId) UseSkillData()
+    {
+        return (Cost, Id);
+    }
+
+    public int SkillDamageCalcByUpgrade()
+    {
+        int nowDamage = Damage;
+
+        if (NowUpgradeList != null && NowUpgradeList.Count > 0) 
+        {
+            NowUpgradeList.ForEach(damageUp => { 
+                if(damageUp == SkillUpgradeType.Damage)
+                    nowDamage = nowDamage * 150 / 100;
+            });
+        }
+
+        return nowDamage;
+    }
 }
---
+++ Assets/JJH/Skill/SkillData.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using UnityEngine;
+using static SkillEnums;
+
+public static class SkillData
+{
+    public static readonly Dictionary<int, SkillBaseStat> BaseSkillData = new()
+    {
+        [1000] = new SkillBaseStat(
+            // 아이디
+            1000,
+            // 이름
+            "단일1",
+            // 스킬 설명
+            "설명1",
+            // 코스트
+            1,
+            // 피해량
+            10,
+            // 가능한 업그레이드
+            new List<SkillUpgradeType> { 
+                SkillUpgradeType.Damage,
+                SkillUpgradeType.DownCost,
+                SkillUpgradeType.LifeStill  }
+            ,
+            // 타겟 지정 종류
+            SkillTargetType.Single 
+        ),
+        [1001] = new SkillBaseStat(
+            1001,
+            "단일2",
+            "설명2",
+            2,      
+            20,     
+            new List<SkillUpgradeType> {
+                SkillUpgradeType.Damage,
+                SkillUpgradeType.DownCost,
+                SkillUpgradeType.LifeStill  }
+            ,                     
+            SkillTargetType.Single
+        ),
+        [1002] = new SkillBaseStat(
+            1002,
+            "광역1", 
+            "설명3", 
+            3, 
+            15, 
+            new List<SkillUpgradeType> {
+                SkillUpgradeType.Damage,
+                SkillUpgradeType.DownCost,
+                SkillUpgradeType.LifeStill  }
+            ,                   
+            SkillTargetType.Area
+        ),
+    };
+}
