aba6957583cf90b4e90b128642e9ac9f180bf476
Enzeun <khuynjun123@naver.com>
2026-08-03 10:55:26 +0900

    [feat] 턴 매니저 기본로직 추가

--- Assets/HJ/Script/Manager/TurnManager.cs
+++ Assets/HJ/Script/Manager/TurnManager.cs
@@ -1,16 +1,55 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TurnManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    // 턴 매니저 싱글턴 적용
+    public static TurnManager instance;
+
+    private void Awake()
     {
-        
+        if (instance != null && instance != this.gameObject)
+        {
+            Destroy(gameObject);
+        }
+
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    //===============================================================================================
+
+    // 턴 매니저의 상태 머신
+    public enum TurnState
     {
-        
+        Wait, // 아무 상태 아닐 때.
+        StartBattle, // 배틀이 시작되는 시점
+        PlayerTurnStart, // 플레이어 턴 시작
+        PlayerPlanning, // 스킬 등록을 여기서 함.
+        ExecuteSkills, // 등록된 스킬 사용
+        PlayerTurnEnd, // 플레이어 턴 종료
+        CheckBattleState, // 전투가 지속 가능한 상황인지 체크
+        EnemyTurn, // 적 턴
+        EnemyTurnEnd, // 적 턴 종료
+        EndBattle, // 배틀 종료 (전체 배틀이 종료된 상태)
     }
+
+    public TurnState turnState { get; private set; } = TurnState.Wait; // 초기상태로 시작
+
+    //===============================================================================================
+
+    /// <summary>
+    /// 전투매니저 초기화 (배틀 시작 시 호출)
+    /// </summary>
+    public void InitializeBattle()
+    {
+        turnState = TurnState.Wait;
+    }
+
+    //===============================================================================================\
+      
+    // 플레이어의 스킬을 큐 형식으로 저장함
+    Queue<int> skillQueue = new ();
+
+
 }
