da66dbb631e186f96be163d23b25f6fe3b156e66
Enzeun <khuynjun123@naver.com>
2026-08-04 09:43:42 +0900

    [feat] 플레이어/적 UI 구분

--- Assets/HJ/Script/Manager/BattleUIManager.cs
+++ Assets/HJ/Script/Manager/BattleUIManager.cs
@@ -1,43 +1,62 @@
+using DamageNumbersPro;
+using DG.Tweening;
 using Sirenix.OdinInspector;
 using TMPro;
-using UnityEngine;
 using Unity.UI;
+using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
-using DamageNumbersPro;
+using static UnityEngine.Rendering.DebugUI;
 
 public class BattleUIManager : MonoBehaviour
 {
-    [SerializeField, Required, BoxGroup("**참조필요!**")]
+    [SerializeField, Required, BoxGroup("**참조필요!**플레이어")]
     private TextMeshProUGUI playerHpBarText;
-    [SerializeField, Required, BoxGroup("**참조필요!**")]
+    [SerializeField, Required, BoxGroup("**참조필요!**플레이어")]
     private Slider playerHpSlider;
-    [SerializeField, Required, BoxGroup("**참조필요!**")]
-    private DamageNumber numberPrefab;
-    [SerializeField, Required, BoxGroup("**참조필요!**")]
-    private RectTransform rectParent;
+    [SerializeField, Required, BoxGroup("**참조필요!**플레이어")]
+    private DamageNumber player_NumberPrefab;
+    [SerializeField, Required, BoxGroup("**참조필요!**플레이어")]
+    private RectTransform PlayerHP_Number_Location;
+
+    [SerializeField, Required, BoxGroup("**참조필요!**적")]
+    private DamageNumber enemy_NumberPrefab;
+
+
 
     private PlayerBaseStat playerStat;
 
-
-
-
-    [BoxGroup("UI Debug"), Button]
-    private void PlayerTakeDamageUI(int currentHp, int damage)
+    private void OnEnable()
     {
-        int max = 300;
+        playerStat.OnHpChanged += PlayerHpChangedUI;
+        playerStat.OnDamagedTaken += TakeDamage_UI_NumberAnimation;
+    }
 
-        float value = (float)currentHp / max;
-
-        playerHpBarText.text = $"{currentHp}/{max}";
-
-        PlayDamagedUIAnimation(damage, value);
+    private void OnDisable()
+    {
+        playerStat.OnHpChanged -= PlayerHpChangedUI;
+        playerStat.OnDamagedTaken -= TakeDamage_UI_NumberAnimation;
     }
 
     [BoxGroup("UI Debug"), Button]
-    private void PlayDamagedUIAnimation(int damage, float endValue)
+    private void PlayerHpChangedUI(int currentHp, int maxHp)
     {
-        DamageNumber damageNumber = numberPrefab.SpawnGUI(rectParent, Vector2.zero, damage);
+        float endValue = (float)currentHp / maxHp;
+
+        playerHpBarText.text = $"{currentHp}/{maxHp}";
+
+        HP_Decrease_UIAnimation(endValue);
+
+    }
+
+    private void TakeDamage_UI_NumberAnimation(int damage)
+    {
+        DamageNumber damageNumber = player_NumberPrefab.SpawnGUI(PlayerHP_Number_Location, Vector2.zero, damage);
+
+    }
+
+    [BoxGroup("UI Debug"), Button]
+    private void HP_Decrease_UIAnimation(float endValue)
+    {
 
         playerHpSlider.transform.DOShakePosition(0.2f, 10f, 90);
         playerHpSlider.DOValue(endValue, 0.1f);
