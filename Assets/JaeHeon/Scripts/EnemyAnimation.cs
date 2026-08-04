16b1fa82dc0afe7b5ca2f94be02f956e21eff496
HaJaeHeon <88596006+HaJaeHeon@users.noreply.github.com>
2026-08-04 16:28:29 +0900

    [Feat] EnemyAnimation
    [Feat] EnemyAnimation - DoTween으로 플레이어에게 다가가서 공격하거나 멀리서 공격 할 때 플레이어를 바라보고 공격함

--- Assets/JaeHeon/Scripts/EnemyAnimation.cs
+++ Assets/JaeHeon/Scripts/EnemyAnimation.cs
@@ -1,25 +1,45 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using DG.Tweening;
+using Sirenix.OdinInspector;
 
 public class EnemyAnimation : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-    [SerializeField] private GameObject player;
-    //[SerializeField] private EnemyBase enemyBase;
-
+    //[SerializeField] private GameObject player;
+    [SerializeField] private EnemyBase enemyBase;
+    [SerializeField] Transform targetTransform;
+    [SerializeField] float enemyMoveSpeed = 1f;
+    [SerializeField] float enemyMagicSpeed = 0.4f;
 
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private Transform EmissionTransform;
-    [SerializeField] private float projectileSpeed = 5f;
+    [SerializeField] private float projectileSpeed = 20f;
 
     [SerializeField] private GameObject spellPrefab;
     [SerializeField] private GameObject buffPrefab;
+    [SerializeField] private float distanceOfPlayerAtNormalAttack = 2f;
+
+    Transform currentTransform;
+    Quaternion currentRotation;
+
+    /// <summary>
+    /// doRotation, doLookAt할 때 걸리는 시간은 임의적으로 집어넣었음
+    /// </summary>
 
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-        //enemyBase = GetComponent<EnemyBase>();
+        enemyBase = GetComponent<EnemyBase>();
+    }
+
+    private void Start()
+    {
+        targetTransform = enemyBase.playerTransform;
+
+        currentTransform = transform;
+        currentRotation = transform.rotation;
     }
 
     public void PlayerTakeDamage()
@@ -33,47 +53,90 @@ public class EnemyAnimation : MonoBehaviour
         animator.SetTrigger("TakeDamage");
     }
 
-    [ContextMenu("AnimationShootProjectile")]
+    [Button]
     public void EnemyShootProjectile(Transform targetTransform)
     {
-        animator.SetTrigger("ProjectileAttack");
+        Vector3 direction = targetTransform.position - transform.position;
+
+        Sequence seq = DOTween.Sequence();
+        seq.Append(transform.DOLookAt(targetTransform.position, 0.4f));
+        seq.AppendInterval(0.4f);
+        seq.AppendCallback(() =>
+        {
+            animator.SetTrigger("ProjectileAttack");
+        });
+        seq.AppendInterval(1f);
+        seq.Append(transform.DORotate(currentRotation.eulerAngles, 0.4f));
     }
 
     //
     //몬스터 방향을 먼저 플레어이 방향으로 돌리는 것도 필요한가
     //target 위치로 발사체 발사 (발사체 방향도 target 방향으로)
-    [ContextMenu("EmissionProjectile")]
-    public void EventEnemyEmissionProjectile(Transform targetTransform)
+    public void EventEnemyEmissionProjectile()
     {
         GameObject projectile = GameObject.Instantiate(projectilePrefab);
         projectile.transform.position = EmissionTransform.position;
 
-        Vector3 direction = (targetTransform.position - projectile.transform.position).normalized;
+        Vector3 direction = (targetTransform.position - EmissionTransform.position).normalized;
         projectile.transform.forward = direction;
 
         Rigidbody rb = projectile.GetComponent<Rigidbody>();
         rb.AddForce(direction * projectileSpeed, ForceMode.Impulse);
     }
 
-    public void EnemyNormalAttack()
+    [Button]
+    public void EnemyNormalAttack(Transform targetTransform)
     {
-        animator.SetTrigger("StingAttack");
+        Vector3 direction = (targetTransform.position - transform.position).normalized;
+
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append(transform.DOLookAt(targetTransform.position,0.4f));
+
+        seq.Append(transform.DOMove((targetTransform.position - direction * distanceOfPlayerAtNormalAttack), enemyMoveSpeed));
+
+        seq.AppendCallback(()=>
+        {
+            animator.SetTrigger("StingAttack");
+        });
+
+        seq.AppendInterval(1f);
+        seq.Append(transform.DOMove(currentTransform.position, enemyMoveSpeed));
+        seq.Append(transform.DORotate(currentRotation.eulerAngles, 0.4f));
     }
+    [Button]
     public void EnemyCastSpell(Transform targetTransform)
     {
+        Sequence seq = DOTween.Sequence();
+
         animator.SetTrigger("CastSpell");
+        seq.AppendInterval(enemyMagicSpeed);
+        seq.AppendCallback(() =>
+        {
+            GameObject spell = GameObject.Instantiate(spellPrefab);
+            spell.transform.position = targetTransform.position;
+        });
     }
 
-    public void EventEnemyCastSpell(Transform targetTransform)
+    public void EventEnemyCastSpell()
     {
+        //해당 위치에서 플레이어 damage 받는 함수 제작
         //player.TakeDamage(enemyBase.attackPower);
     }
 
-    
 
+    [Button]
     public void EnemyBuff()
     {
+        Sequence seq = DOTween.Sequence();
         animator.SetTrigger("Buff");
+        seq.AppendInterval(enemyMagicSpeed);
+        seq.AppendCallback(() =>
+        {
+            GameObject buffEffect = GameObject.Instantiate(buffPrefab);
+            buffEffect.transform.position = transform.position;
+        });
+        
     }
 
     public void EnemyDie()
