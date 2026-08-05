cd2106918857a175c6cd99e9d99e1e3aa882626e
RGovernment <117322410+RGovernment@users.noreply.github.com>
2026-08-05 10:06:20 +0900

    [fix] 스킬 생성 및 제어 버그 수정
    1. 투사체 생성 시 서로 충돌해 사라지는 현상 수정
      ㄴ 1-2. 투사체 생성 시 서로 밀려나 회전하는 현상도 함께 수정
    2. 투사체 생성 시 SkillEffectSpawner가 존재하지 않아 event가 null이 되어 작동하지 않는 현상 수정

--- Assets/JJH/SkillEnums.cs
+++ Assets/JJH/SkillEnums.cs
@@ -5,5 +5,6 @@ public static class SkillEnums
 {
     public enum SkillUpgradeType { Damage, DownCost, LifeStill }
     public enum SkillTargetType { Single, Area }
+    public enum SkillTargetSubType { Single, Multi }
     public enum SkillPoseType { Instance, Horizontal, Vertical, Stomp, LongCast }
 }
