1810b90d55ea2433977462204e19580a1d67b447
RGovernment <117322410+RGovernment@users.noreply.github.com>
2026-08-05 12:41:40 +0900

    [추가] 버튼 DoTween 추가
    1. 버튼이 시작시 움직이도록 추가

---
+++ Assets/JJH/Title/TitleManager.cs
@@ -0,0 +1,138 @@
+using DG.Tweening;
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using SF = UnityEngine.SerializeField;
+
+public class TitleManager : MonoBehaviour
+{
+    [Header("타이틀")]
+    [SF] private Image titleImage;
+    [SF] private Image titleImageBackground;
+    [SF] private TextMeshProUGUI titleText;
+
+    [Header("버튼 그룹")]
+    [SF] private VerticalLayoutGroup btnGroup;
+
+    [Header("측면 바")]
+    [SF] private Image sideBar;
+    [SF] private CanvasGroup sideBarCanvasGroup;
+
+    [Header("시작 버튼")]
+    [SF] private Button startBtn;
+    [SF] private RectTransform startBtnRect;
+    [SF] private CanvasGroup startBtnCanvasGroup;
+    [SF] private TextMeshProUGUI startBtntext;
+
+    [Header("종료 버튼")]
+    [SF] private Button exitBtn;
+    [SF] private RectTransform exitBtnRect;
+    [SF] private CanvasGroup exitBtnCanvasGroup;
+    [SF] private TextMeshProUGUI exitBtntext;
+
+    [Header("기타")]
+    [SF] private AnimationCurve barEase;
+    public void Start()
+    {
+        StartCoroutine(nameof(StartEvent));
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I)) StartCoroutine(nameof(StartEvent));
+    }
+
+    public IEnumerator StartEvent()
+    {
+        // 상태 초기화
+        startBtnCanvasGroup.gameObject.SetActive(false);
+        exitBtnCanvasGroup.gameObject.SetActive(false);
+        btnGroup.gameObject.SetActive(false);
+        sideBar.gameObject.SetActive(false);
+
+        // 초기 설정
+        startBtnCanvasGroup.gameObject.SetActive(true);
+        exitBtnCanvasGroup.gameObject.SetActive(true);
+        sideBar.gameObject.SetActive(true);
+        sideBarCanvasGroup.alpha = 0;
+        startBtnCanvasGroup.alpha = 0;
+        exitBtnCanvasGroup.alpha = 0;
+        btnGroup.gameObject.SetActive(true);
+        
+        yield return null;
+        btnGroup.enabled = false;
+
+        Vector2 startVectorSave = startBtnRect.anchoredPosition;
+        Vector2 exitVectorSave = exitBtnRect.anchoredPosition;
+        Vector2 sideBarSzie = sideBar.rectTransform.sizeDelta;
+        
+        sideBar.rectTransform.sizeDelta = new(sideBarSzie.x, 0);
+        startBtnRect.anchoredPosition = new(startVectorSave.x + 400, startVectorSave.y);
+        exitBtnRect.anchoredPosition = new(exitVectorSave.x + 400, exitVectorSave.y);
+
+        startBtntext.maxVisibleCharacters = 0;
+        exitBtntext.maxVisibleCharacters = 0;
+
+        float duration = 0.025f;
+        float startTextDuration = duration * startBtntext.textInfo.characterCount;
+        float exitTextduration = duration * exitBtntext.textInfo.characterCount;
+
+        float btnAnimationTime = 0.27f;
+        float btnDelayTime = 0.08f;
+        float btnCount = 2;
+
+        float sideBarTime = btnAnimationTime + ((btnAnimationTime - btnDelayTime) * btnCount);
+
+        var title = DOTween.Sequence();
+
+
+
+        yield return title;
+
+        var seq = DOTween.Sequence();
+
+        seq.Join(sideBar.rectTransform.DOSizeDelta(sideBarSzie, sideBarTime).SetEase(barEase));
+        seq.Join(sideBarCanvasGroup.DOFade(1, sideBarTime).SetEase(barEase));
+
+        float startBtnStartTime = 0f;
+        seq.Insert(startBtnStartTime, startBtnRect.DOAnchorPosX(startVectorSave.x, btnAnimationTime).SetEase(Ease.Linear));
+        seq.Insert(startBtnStartTime, startBtnCanvasGroup.DOFade(1, btnAnimationTime).SetEase(Ease.Linear));
+        seq.Insert(btnAnimationTime, DOTween.To(
+            () => startBtntext.maxVisibleCharacters,
+            x => startBtntext.maxVisibleCharacters = x,
+            startBtntext.textInfo.characterCount,
+            startTextDuration
+        ).SetEase(Ease.Linear));
+
+        float exitBtnStartTime = btnDelayTime;
+        seq.Insert(exitBtnStartTime, exitBtnRect.DOAnchorPosX(exitVectorSave.x, btnAnimationTime).SetEase(Ease.Linear));
+        seq.Insert(exitBtnStartTime, exitBtnCanvasGroup.DOFade(1, btnAnimationTime).SetEase(Ease.Linear));
+        seq.Insert(btnAnimationTime + exitBtnStartTime, DOTween.To(
+            () => exitBtntext.maxVisibleCharacters,
+            x => exitBtntext.maxVisibleCharacters = x,
+            exitBtntext.textInfo.characterCount, 
+            exitTextduration)
+            .SetEase(Ease.Linear));
+
+        seq.OnComplete(() => { btnGroup.enabled = true; });
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("FirstScene");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
